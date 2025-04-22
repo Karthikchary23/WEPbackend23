05b71f2b911ac19894ee1bb38d347e06dc4cb86a
Karthikcahry23 <lingojikarthikchary@gmail.com>
2025-04-22 19:10:35 +0530

    verification

--- socket.js
+++ socket.js
@@ -1,93 +1,100 @@
 const { Server } = require("socket.io");
 
 let io;
-const serviceProviders = {}; 
+const serviceProviders = {};
 function init(server) {
-    io = new Server(server, {
-        cors: {
-            origin: "*",
-        },
+  io = new Server(server, {
+    cors: {
+      origin: "*",
+    },
+  });
+
+  io.on("connection", (socket) => {
+    // console.log("New provider connected:", socket.id);
+
+    // Register service provider when they connect
+    socket.on("registerServiceProvider", (data) => {
+      console.log("data of registered", data);
+      const { email, serviceType } = data;
+
+      // Save the service provider's details
+      serviceProviders[socket.id] = { email, serviceType };
+
+      // Join a room with the service provider's email
+      socket.join(email);
+
+      console.log(`Service provider ${email} joined their room`);
     });
 
-    io.on("connection", (socket) => {
-        // console.log("New provider connected:", socket.id);
+    socket.on("registerCustomer", (customerEmail) => {
+      socket.join(customerEmail);
+      console.log(`Customer ${customerEmail} joined their room`);
+    });
 
-        // Register service provider when they connect
-        socket.on("registerServiceProvider", (data) => {
-            console.log("data of registered", data);
-            const { email, serviceType } = data;
+    socket.on("serviceAccepted", (data) => {
+      console.log("Service Accepted now:", data);
+      console.log("types of data is ", typeof data);
+      io.to(data.customerEmail).emit("notification", data);
+    });
 
-            // Save the service provider's details
-            serviceProviders[socket.id] = { email, serviceType };
+    socket.on("cancelRequest", (data) => {
+      const { customerEmail, providerEmail } = data;
+      console.log("Cancel request received:", data);
 
-            // Join a room with the service provider's email
-            socket.join(email);
-
-            console.log(`Service provider ${email} joined their room`);
+      // Notify the customer if the service provider cancels
+      if (customerEmail) {
+        console.log("Notifying customer:", customerEmail);
+        io.to(customerEmail).emit("requestCanceledbyprovider", {
+          providerEmail,
         });
+      }
 
-        socket.on("registerCustomer", (customerEmail) => {
-            socket.join(customerEmail);
-            console.log(`Customer ${customerEmail} joined their room`);
-          });
-          
-        socket.on("serviceAccepted", (data) => {
-            console.log("Service Accepted now:", data);
-            console.log("types of data is ",typeof(data))
-            io.to(data.customerEmail).emit("notification", data); 
-          });
+      // Notify the service provider if the customer cancels
+      if (providerEmail) {
+        console.log("Notifying provider:", providerEmail);
+        io.to(providerEmail).emit("requestCanceledbycutomer", {
+          customerEmail,
+        });
+      }
 
-        socket.on("cancelRequest", (data) => {
-            const { customerEmail, providerEmail } = data;
-            console.log("Cancel request received:", data);
-          
-            // Notify the customer if the service provider cancels
-            if (customerEmail) {
-                console.log("Notifying customer:", customerEmail);
-                io.to(customerEmail).emit("requestCanceledbyprovider", { providerEmail });
-            }
-          
-            // Notify the service provider if the customer cancels
-            if (providerEmail) {
-                console.log("Notifying provider:", providerEmail);
-                io.to(providerEmail).emit("requestCanceledbycutomer", { customerEmail });
-            }
-          
-            console.log("Request canceled:", data);
-          });
+      console.log("Request canceled:", data);
+    });
 
-         socket.on("providercurrentlocation", (data) => {
-    console.log("Provider current location received:", data);
-    const { location, customerEmail } = data;
+    socket.on("providercurrentlocation", (data) => {
+      console.log("Provider current location received:", data);
+      const { location, customerEmail } = data;
 
-    // Notify the customer with the provider's current location
-    if (customerEmail) {
+      // Notify the customer with the provider's current location
+      if (customerEmail) {
         io.to(customerEmail).emit("providerLocationUpdate", location);
         console.log(`Location sent to customer ${customerEmail}:`, location);
-    } else {
+      } else {
         console.log("Customer email is missing in the data.");
-    }
-});
-
-        // Remove provider when they disconnect
-        socket.on("disconnect", () => {
-            console.log("Provider disconnected:", socket.id);
-            delete serviceProviders[socket.id];
-        });
-
-        
+      }
     });
+    socket.on("serviceAccepted", (data) => {
+      console.log("Service Accepted now:", data);
+     
+      io.to(data.serviceProviderEmail).emit("serviceAcceptednotification", data);
+    });
+
+    // Remove provider when they disconnect
+    socket.on("disconnect", () => {
+      console.log("Provider disconnected:", socket.id);
+      delete serviceProviders[socket.id];
+    });
+  });
 }
 
 function getIo() {
-    if (!io) {
-        throw new Error("Socket.io not initialized");
-    }
-    return io;
+  if (!io) {
+    throw new Error("Socket.io not initialized");
+  }
+  return io;
 }
 
 function getServiceProviders() {
-    return serviceProviders;
+  return serviceProviders;
 }
 
 // ✅ Make sure we export everything correctly
